9b9b781624949fbd19ce6bd6dd6f01178287f1a1
angeleitor4000 <angelguardenogomez@gmail.com>
2024-04-24 21:00:20 +0200

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Dentro from "./pages/Dentro";
 import PruebaRuta from "./pages/PruebaRuta";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element= {<LandingPage/>}/>
-        <Route path="https://front-spotify-manager-angeleitor4000.onrender.com/dentro" element = {<Dentro/>} />
-        <Route path="https://front-spotify-manager-angeleitor4000.onrender.com/pruebar" element = {<PruebaRuta/>} />
+        <Route path="/dentro" element = {<Dentro/>} />
+        <Route path="/pruebar" element = {<PruebaRuta/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     
   );
 }
