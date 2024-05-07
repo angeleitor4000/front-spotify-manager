afa0e6c46610a816e00a939a162f99e79862b13f
angeleitor4000 <angelguardenogomez@gmail.com>
2024-05-07 16:20:04 +0200

    Elevar estados a App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,50 @@ import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import PruebaRuta from "./pages/PruebaRuta";
 import ErrorPage from "./pages/ErrorPage";
+import { useState, useEffect } from "react";
 import "./styles/App.css";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState([]);
+  const [actualTrack, setActualTrack] = useState([]);
+
+  useEffect(() => {
+    getCurrentUser();
+    getActualTrack();
+  }, []);
+
+  async function getCurrentUser() {
+    try {
+      const response = await fetch("http://localhost:3000/user");
+      const data = await response.json();
+      setCurrentUser(data);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  }
+
+  async function getActualTrack() {
+    try {
+      const response = await fetch("http://localhost:3000/getactualtrack");
+      const data = await response.json();
+      setActualTrack(data);
+    } catch (error) {
+      console.error("Error fetching playlist:", error);
+    }
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element= {<LandingPage/>}/>
-        <Route path="/home" element = {<Home/>} />
-        <Route path="/pruebar" element = {<PruebaRuta/>} />
-        <Route path="/*" element = {<ErrorPage/>}/>
+        <Route index element={<LandingPage />} />
+        <Route
+          path="/home"
+          element={<Home currentUser={currentUser} actualTrack={actualTrack} />}
+        />
+        <Route path="/pruebar" element={<PruebaRuta />} />
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
-    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,83 +3,46 @@ import { useState, useEffect } from "react";
 import Escuchando from "../components/Escuchando";
 import Hola from "../components/Hola";
 
+export default function Home({ currentUser, actualTrack }) {
+  const [cargado, setCargado] = useState(true);
 
-export default function Dentro() {
-
-    const [actualTrack, setActualTrack] = useState([]);
-    const [currentUser, setCurrentUser] = useState([]);
-    const [cargado, setCargado] = useState(true);
-
+  useEffect(() => {
     if (cargado) {
-        setCargado(false)
-        refreshlogin()
-        currentuser()
+      setCargado(false);
+      refreshLogin();
     }
-    async function refreshlogin() {
-        try {
-            const response = await fetch("http://localhost:3000/refreshlogin");
-            if (response.redirected) {
-                // Redirigir al URL indicado por el servidor
-                window.location.href = response.url;
-            } else {
-                console.log("Credentials refreshed successfully");
-            }
-        } catch (error) {
-            console.error('Error refreshing credentials: ', error);
-        }
+  }, [cargado]);
+
+  async function refreshLogin() {
+    try {
+      const response = await fetch("http://localhost:3000/refreshlogin");
+      if (response.redirected) {
+        // Redirigir al URL indicado por el servidor
+        window.location.href = response.url;
+      } else {
+        console.log("Credentials refreshed successfully");
+      }
+    } catch (error) {
+      console.error("Error refreshing credentials: ", error);
     }
-    
+  }
 
-    async function getActualTrack() {
-        try {
-            const response = await fetch("http://localhost:3000/getactualtrack");
-            const data = await response.json();
-            setActualTrack(data);
-        } catch (error) {
-            console.error('Error fetching playlist:', error);
-        }
-    }
+  return (
+    <div>
+      <h1>Dentro</h1>
+      <a href="/">
+        <button>VOLVER</button>
+      </a>
 
-    async function currentuser() {
-        try {
-            const response = await fetch("http://localhost:3000/user");
-            const data = await response.json();
-            setCurrentUser(data);
-            await timeout(2000);
-        } catch (error) {
-            console.error('Error fetching user:', error);
-        }
-    }
-
-    useEffect(() => {
-        getActualTrack();
-        const intervalId = setInterval(() => {
-            getActualTrack();
-        }, 500);
-        return () => clearInterval(intervalId);
-    }, []);
-
-    function timeout(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-    return (
-        <div>
-            <h1>Dentro</h1>
-            <a href="/"><button>VOLVER</button></a>
-
-            <div className="container">
-
-                <div className="izquierda">
-                    <Hola currentUser={currentUser} />
-                </div>
-                <div className="cuerpo">
-                    {/* Contenido del cuerpo */}
-                </div>
-                <div className="derecha">
-                    <Escuchando actualTrack={actualTrack} />
-                </div>
-            </div>
+      <div className="container">
+        <div className="izquierda">
+          <Hola currentUser={currentUser} />
         </div>
-    );
+        <div className="cuerpo">{/* Contenido del cuerpo */}</div>
+        <div className="derecha">
+          <Escuchando actualTrack={actualTrack} />
+        </div>
+      </div>
+    </div>
+  );
 }
