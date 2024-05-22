8cb780a77a16fa4e22abdc4d5c0c4e14d5e74db5
angeleitor4000 <angelguardenogomez@gmail.com>
2024-05-22 08:40:11 +0200

    Arreglado perfil vacio

--- src/components/Hola.js
+++ src/components/Hola.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "../styles/HolaEscuchandoEstilo.css";
+import perfilVacio from "../images/perfilVacio.jpg"
 
 export default function Hola(props) {
     const { currentUser } = props;
+
+    const getUserImage = (images) => {
+        if (!images || images.length === 0) return null;
+        return images[1] ? images[1].url : images[0].url; // Fallback al primer índice si el segundo no existe
+    };
 
     return (
         <div className="hola-escuchando-container">
             <h1>¡Hola!</h1>
 
             <p>{currentUser ? currentUser.display_name : ''}</p>
-            {currentUser && currentUser.images && (
-                <img src={currentUser.images[1].url} alt="" className="imagen-hola"></img>
+            {currentUser && getUserImage(currentUser.images) ? (
+                <img src={getUserImage(currentUser.images)} alt="Perfil" className="imagen-hola"></img>
+            ) : (
+                <img src={perfilVacio} alt="Perfil" className="imagen-hola"></img>
             )}
-
-            {currentUser && !currentUser.images && 'sin imagen'}
-            
         </div>
     );
 }
